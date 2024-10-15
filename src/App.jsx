4c4e5624a3e6d2e570c100d5dbb57bb338fb96f1
Josh Stringer <jstringer1212@gmail.com>
2024-10-15 18:23:15 -0500

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,18 @@
+import React, { useState } from "react";
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
 
+export default function App() {
+  const [token, setToken] = useState(null);
+  return (
+    <>
+    
+      <Authenticate token={token} setToken={setToken} />
+      <SignUpForm token={token} setToken={setToken}  />
+    
+    </>
+  );
+}
+
+
+// Object { message: "ENOENT: no such file or directory, stat '/app/index.html'", name: "Error" }
